72deacf8ce8fe53087c3e4cb02c379d89d8fef42
Johathan29 <rosariojohathan@gmail.com>
2026-02-06 16:47:30 -0400

    rutas

--- src/pages/updates/index.tsx
+++ src/pages/updates/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Layout from '@theme/Layout'
 
-import UpdatesList from '../../components/UpdatesSection/UpdatesList'
+import UpdatesList from '@site/src/components/UpdatesSection/UpdatesList'
 import { useLocation } from 'react-router-dom'
-import UpdateSlugPage from '../../components/UpdatesSection/UpdateSlugPage'
+import UpdateSlugPage from '@site/src/components/UpdatesSection/UpdateSlugPage'
 
 export default function UpdatesPage() {
     const location = useLocation()
